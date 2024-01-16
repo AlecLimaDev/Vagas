0579b1c02e2d07b73a194e8447389615c5c5b613
Alec Lima <aleclimadev@gmail.com>
2024-01-16 15:43:28 -0300

    feat: vagas instance config

--- src/helpers/vagas.ts
+++ src/helpers/vagas.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiVagas = axios.create({
-  baseURL: "https://api.github.com/repos/frontendbr/vagas/issues",
+  baseURL: "https://api.github.com/repos",
 });
 
 export default apiVagas;
