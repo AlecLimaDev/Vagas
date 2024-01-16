a5f14626ec9573a47a04610259fb1b169ff68633
Alec Lima <aleclimadev@gmail.com>
2024-01-16 15:47:28 -0300

    chore: GlobalStyle config with styled-components

---
+++ src/style/globalStyle.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+        body {
+            margin: 0;
+            padding: 0;
+            background: #292929;
+            
+        }
+`;
+export default GlobalStyle;
