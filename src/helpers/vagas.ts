0cff3f380071970b50bbcb1ecabd6ba4ac0e66c7
Alec Lima <aleclimadev@gmail.com>
2024-01-17 21:08:07 -0300

    feat: api base url

--- src/helpers/vagas.ts
+++ src/helpers/vagas.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const apiVagas = axios.create({
   baseURL: "https://api.github.com/repos",
+  params: {
+    token: "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"
+  }
 });
 
 export default apiVagas;
