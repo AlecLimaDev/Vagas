b60ce8e089d8686a3e214d35b65275f8b0a72d40
Alec Lima <aleclimadev@gmail.com>
2024-01-17 21:42:24 -0300

    bugfix

--- src/pages/Vagas/hooks/useVagas.ts
+++ src/pages/Vagas/hooks/useVagas.ts
@@ -3,49 +3,26 @@ import apiVagas from "../../../helpers/vagas";
 import { AxiosError } from "axios";
 
 interface Job {
-  numero: number;
-  state: string;
-  locked: false;
-  assignee: null;
-  created_at: string;
-  updated_at: string;
-  closed_at: null;
-  url: string;
-  repository_url: string;
-  labels_url: string;
-  comments_url: string;
-  events_url: string;
   html_url: string;
-  id: number;
-  node_id: number;
+  id: string;
+  node_id: string;
   number: number;
   title: string;
   user: {
     login: string;
-    id: number;
-    node_id: number;
     avatar_url: string;
-    gravatar_id: string;
-    url: string;
+    gravatar_id: number;
     html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
     type: string;
   };
   labels: {
-    id: number;
-    node_id: number;
-    url: string;
     name: string;
     color: string;
   };
+  state: string;
+  comments: number;
+  created_at: string;
+  updated_at: string;
 }
 
 const useVagas = () => {
--- src/pages/Vagas/index.tsx
+++ src/pages/Vagas/index.tsx
@@ -3,100 +3,65 @@ import useVagas from "./hooks/useVagas";
 import * as S from "./style";
 import { FaGithub } from "react-icons/fa";
 
-export interface Job {
-  url: string;
-  repository_url: string;
-  labels_url: string;
-  comments_url: string;
-  events_url: string;
+interface Job {
   html_url: string;
   id: string;
   node_id: string;
   number: number;
   title: string;
   user: {
-    login: string;
-    id: string;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: number;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
+  login: string;
+  avatar_url: string;
+  gravatar_id: number;
+  html_url: string;
+  type: string;
   };
   labels: {
-    name: string;
-    color: string;
+  name: string;
+  color: string;
   };
-
   state: string;
-  locked: boolean;
-  assignee: null;
-  milestone: null;
   comments: number;
   created_at: string;
   updated_at: string;
-  closed_at: null;
-  author_association: string;
-  active_lock_reason: string;
-  body: string;
-  reactions: {
-    url: string;
-    total_count: number;
-    laugh: number;
-    hooray: number;
-    confused: number;
-    heart: number;
-    rocket: number;
-    eyes: number;
-  };
 }
+  
 
-const Vagas = () => {
-  const { filteredJob } = useVagas();
-  return (
-    <>
-      <S.Section>
-        <Input />
-      </S.Section>
-      <S.Wrapper>
-        {filteredJob.map((jobs: Job, index) => (
-          <>
-            <S.Container key={index}>
+  const Vagas = () => {
+    const { filteredJob } = useVagas();
+  
+    return (
+      <>
+        <S.Section>
+          <Input />
+        </S.Section>
+        <S.Wrapper>
+          {filteredJob.map((job: Job) => (
+            <S.Container key={job.id}>
               <div>
-                <img src={jobs.user.avatar_url} alt={jobs.user.login} />
-                <h2>{jobs.user.login}</h2>
+                <img src={job.user.avatar_url} alt={job.user.login} />
+                <h2>{job.user.login}</h2>
               </div>
               <section>
-                <h2>{jobs.title}</h2>
-                <h3>Comentários: {jobs.comments}</h3>
-                <h4>{jobs.created_at}</h4>
-                <h4>{jobs.updated_at}</h4>
+                <h2>{job.title}</h2>
+                <h3>Comentários: {job.comments}</h3>
+                <h4>{job.created_at}</h4>
+                <h4>{job.updated_at}</h4>
               </section>
               <section>
-                <a href={jobs.html_url} target="_blank">
+                <a href={job.html_url} target="_blank">
                   <button>
                     Ver vaga no GitHub
                     <FaGithub />
                   </button>
                 </a>
-                <p>{jobs.labels.color}</p>
+                <p>{job.labels.color}</p>
               </section>
             </S.Container>
-          </>
-        ))}
-      </S.Wrapper>
-    </>
-  );
-};
-
-export default Vagas;
+          ))}
+        </S.Wrapper>
+      </>
+    );
+  };
+  
+  export default Vagas;
